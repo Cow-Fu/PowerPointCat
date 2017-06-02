33f13ae9beb6fb2151e29e9050be3bbb7a241c8c
Nathan Justinian <nathanj758@gmail.com>
2017-06-02 17:09:31 -0400

    MarkupWalker impl for powerpoint slides

--- PowerPoint/SlideBuilder.py
+++ PowerPoint/SlideBuilder.py
@@ -1,40 +1,39 @@
+from .MarkupWalker import MarkupWalker
+from .Slide import Slide
 from bs4 import BeautifulSoup
-from Slide import Slide
 
-events = {}
+mw = MarkupWalker()
+
 class SlideBuilder:
-    def traverseTree(self, markup):
-        soup = markup
-        if not isinstance(soup, BeautifulSoup):
-            soup = BeautifulSoup(markup, "lxml")
-        keys = events.keys()
-        text = ""
-        for node in soup.recursiveChildGenerator():
-            if node.name in keys:
-                text += events[node.name](node)
-        return text
+    @staticmethod
+    @mw.on("a:t")
+    def handleTextNode(soup):
+        return soup.text
 
-    def build(self, markup):
-        soup = BeautifulSoup(markup, "lxml")
-        sections = list(filter(lambda x: x.name == "p:sp", soup.recursiveChildGenerator()))
+    @staticmethod
+    @mw.on("a:endpararpr")
+    def handleNewLine(soup):
+        return "\n"
 
-        title = traverseTree(sections[0])
-        content = traverseTree(sections[1])
+    @staticmethod
+    def build(markup):
+        result = mw.traverseTree(markup)
+        slideText = ""
+        for x in result:
+            if x == " ":
+                slideText += "\\n"
+            else:
+                slideText += x
+        slide = slideText.split("\n")
 
-        return Slide(title, content)
+        title = slide[0].replace("\\n", "\n")
+        content = slide[1:]
+        content = list(map(lambda x: x.replace("\\n", "\n"), slide[1:]))
 
 
-def on(name):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            print("hi " + name)
-        events[name] = func
-        return wrapper
-    return decorator
+        return Slide(title="".join(title), content="".join(content), markup=markup)
 
-if __name__ == '__main__':
-    @on("Steve")
-    def test():
-        pass
 
-    print(events)
+
+    # def __init__(self):
+    #     self.mw = MarkupWalker()
